1d956a3c4340db7b93c01424ac7bf299f22d1de9
Producer <99910126+SNex21@users.noreply.github.com>
2025-01-17 18:43:12 +0300

    add emoji

--- apps/web/src/pages/(authenticated)/(with-tabbar)/account/SubscriptionSection/SubscriptionSection.tsx
+++ apps/web/src/pages/(authenticated)/(with-tabbar)/account/SubscriptionSection/SubscriptionSection.tsx
@@ -52,7 +52,7 @@ const SubscriptionSection: FC = () => {
         />
         ) : (
           <SubscriptionCard
-          emoji={<СonfettiEmoji size={34} />}
+          emoji={<СonfettiEmoji size={38} />}
           title={`Подписка ${subscription.plan.title}`}
           description={`Действует до ${formatDate(subscription.end_date)}`}
           href={"/subscription"}
