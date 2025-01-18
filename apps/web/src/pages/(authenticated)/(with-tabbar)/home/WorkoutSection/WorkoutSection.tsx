02c6e22b28e9e8561b99ab100ddafd6d582b1c93
Producer <99910126+SNex21@users.noreply.github.com>
2025-01-18 19:34:23 +0300

    fix end of sub

--- apps/web/src/pages/(authenticated)/(with-tabbar)/home/WorkoutSection/WorkoutSection.tsx
+++ apps/web/src/pages/(authenticated)/(with-tabbar)/home/WorkoutSection/WorkoutSection.tsx
@@ -4,6 +4,7 @@ import React, { FC } from "react";
 import styles from "./WorkoutSection.module.scss";
 import cn from "classnames";
 import { Link } from "react-router-dom";
+import { useUser } from "@/providers/AuthProvider/AuthProvider";
 import { useFeatureFlagEnabled } from "posthog-js/react";
 
 interface WorkoutCardProps {
@@ -25,9 +26,12 @@ interface WorkoutCardBlockedProps {
 
 const WorkoutSection: FC = () => {
   const showHardLessonButton = useFeatureFlagEnabled("hard-lesson-button");
+  const user = useUser();
 
   return (
-    <section className="wrapper">
+    <>
+    {!user.subscription ? 
+    (<section className="wrapper">
       <div className={styles.section}>
         <WorkoutCard
           title="Начать тренировку"
@@ -47,7 +51,31 @@ const WorkoutSection: FC = () => {
           )}
         </div>
       </div>
+    </section>) : (
+      <section className="wrapper">
+      <div className={styles.section}>
+        <WorkoutCardBlocked
+          title="Начать тренировку"
+          description="по всем заданиям"
+          icon={<FlexedBicepsEmoji size={25} />}
+          href={"/lesson"}
+        />
+        <div className={styles.row}>
+          <WorkoutCardBlocked title="Практика ошибок" icon={<WarningEmoji size={25} />} isSm href={"/lesson/mistakes"} />
+          {showHardLessonButton && (
+            <WorkoutCardBlocked
+              title="Самые сложные"
+              icon={<PersonLiftingWeightsEmoji size={25} />}
+              isSm
+              href={"/lesson/hard"}
+            />
+          )}
+        </div>
+      </div>
     </section>
+    )
+}
+  </>
   );
 };
 
