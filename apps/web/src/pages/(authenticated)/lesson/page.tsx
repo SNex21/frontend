813905df91e3636e6d071467171ad41d0b05ae7b
Producer <99910126+SNex21@users.noreply.github.com>
2025-01-27 18:18:34 +0300

    add continue button

--- apps/web/src/pages/(authenticated)/lesson/page.tsx
+++ apps/web/src/pages/(authenticated)/lesson/page.tsx
@@ -1,3 +1,22 @@
+import { SessionBuilder } from "@/pages/(authenticated)/lesson/SessionBuilder.tsx";
+import { useCloudStorage } from "@/lib/twa/hooks";
+import { useQuery } from "@tanstack/react-query";
+import { completeSession, getTasks } from "@/services/api/tasks";
+import { ACCESS_TOKEN_NAME } from "@/services/auth/storage.ts";
+import { AnimatePresence } from "framer-motion";
+import { LessonPageLoading } from "./loading";
+import React from "react";
+import { LessonComplete } from "./LessonComplete";
+import { useParams } from "react-router-dom";
+import { Guess } from "@/models/Session";
+
+const defaultStats = {
+  total: 0,
+  completed: 0,
+  correct: 0,
+  index: 1,
+};
+
 export default function LessonPage() {
   const params = useParams();
   const cloudStorage = useCloudStorage();
@@ -21,12 +40,24 @@ export default function LessonPage() {
     gcTime: 0,
   });
 
-  const restartSession = React.useCallback(() => {
-    setCompleted(false); // Сбросить состояние завершения
-    setStartDate(new Date().getTime()); // Установить новое время начала
-    setStats(defaultStats); // Сбросить статистику
-    refetch(); // Повторно запросить данные
-  }, [refetch]);
+  const complete = React.useCallback(
+    async ({ guesses }: { guesses: Guess[] }) => {
+      if (session && startDate) {
+        try {
+          await completeSession({
+            token: await cloudStorage.getItem(ACCESS_TOKEN_NAME),
+            id: session.id,
+            wastedTime: new Date().getTime() - startDate,
+            guesses,
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      setCompleted(true);
+    },
+    [session, session?.id, startDate],
+  );
 
   React.useEffect(() => {
     if (!startDate) {
@@ -35,54 +66,33 @@ export default function LessonPage() {
   }, [startDate]);
 
   React.useEffect(() => {
-    if (
-      session?.amount &&
-      stats.total === 0 &&
-      stats.completed === 0 &&
-      stats.correct === 0 &&
-      stats.index === 1
-    ) {
+    if (session?.amount && stats.total === 0 && stats.completed === 0 && stats.correct === 0 && stats.index === 1) {
       setStats({ total: session.amount, completed: 0, correct: 0, index: 1 });
     }
   }, [session?.amount, stats]);
 
-  const correctPercentage = React.useMemo(
-    () => Math.round((stats.correct / stats.total) * 100),
-    [stats]
-  );
+  const correctPercentage = React.useMemo(() => Math.round((stats.correct / stats.total) * 100), [stats]);
+
+  // Функция для обновления задания
+  const handleRestart = () => {
+    setCompleted(false); // Сбросить состояние завершения
+    setStartDate(new Date().getTime()); // Установить новое время начала
+    setStats(defaultStats); // Сбросить статистику
+    refetch(); // Повторно запросить данные
+  };
 
   return (
     <AnimatePresence>
       {isLoading && <LessonPageLoading initial key="loading" />}
       {session && !completed && (
-        <SessionBuilder
-          key="session"
-          session={session}
-          stats={stats}
-          setStats={setStats}
-          onComplete={async ({ guesses }) => {
-            if (session && startDate) {
-              try {
-                await completeSession({
-                  token: await cloudStorage.getItem(ACCESS_TOKEN_NAME),
-                  id: session.id,
-                  wastedTime: new Date().getTime() - startDate,
-                  guesses,
-                });
-              } catch (e) {
-                console.error(e);
-              }
-            }
-            setCompleted(true);
-          }}
-          startDate={startDate ?? Date.now()}
-        />
+        <SessionBuilder key="session" session={session} stats={stats} setStats={setStats} onComplete={complete} startDate={startDate ?? Date.now()} />
       )}
       {completed && (
         <LessonComplete
           startDate={startDate ?? Date.now()}
           correctPercentage={correctPercentage}
-          onRestart={restartSession} // Передаем функцию перезапуска
+          topic_id={params.topicId ? Number(params.topicId) : undefined}
+          onRestart={handleRestart} // Передаем метод для перезапуска сессии
         />
       )}
     </AnimatePresence>
