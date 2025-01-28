499064a0891ed62bf3bd12619e57efb39a95b030
Producer <99910126+SNex21@users.noreply.github.com>
2025-01-28 18:32:09 +0300

    fix reload

--- apps/web/src/pages/router.tsx
+++ apps/web/src/pages/router.tsx
@@ -21,6 +21,7 @@ const LessonPage = lazy(() => import("@/pages/(authenticated)/lesson/page.tsx"))
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />}>
+      {console.log('---')}
       <Route
         path="/welcome"
         element={
