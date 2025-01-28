d57e0f84be43a1e0e8dd074c6389b5425619403c
Producer <99910126+SNex21@users.noreply.github.com>
2025-01-28 21:16:46 +0300

    fix set tasks

--- apps/web/src/pages/(authenticated)/lesson/SetLessonPage/SetLessonPage.tsx
+++ apps/web/src/pages/(authenticated)/lesson/SetLessonPage/SetLessonPage.tsx
@@ -3,24 +3,22 @@ import { AnimatePresence } from "framer-motion";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from "./SetLessonPage.module.scss";
-// import cn from "classnames";
-
 
 export default function SetLessonPage() {
   const params = useParams();
 
   return (
     <AnimatePresence>
-
-        <div className={styles.page}>
-        <h2 className={styles.section__heading}>Настрой свою тренировку</h2>
-        <Link to={`/lesson/topic/${params.topicId}`}>
-          <div className={styles.page__buttons}>
-          <Button>НАЧАТЬ ТРЕНИРОВКУ!</Button> {/* Кнопка для начала новой сессии */}
-          </div>
-        </Link>
+      <div className={styles.page}>
+        <div className={styles.page__content}>
+          <h2 className={styles.section__heading}>Настрой свою тренировку</h2>
         </div>
+        <div className={styles.page__buttons}>
+          <Link to={`/lesson/topic/${params.topicId}`}>
+            <Button>НАЧАТЬ ТРЕНИРОВКУ!</Button>
+          </Link>
+        </div>
+      </div>
     </AnimatePresence>
   );
 }
-
