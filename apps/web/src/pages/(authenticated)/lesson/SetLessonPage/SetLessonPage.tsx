6e2fed7edb070c4e96b5b38a510ce5f442d99d5c
Producer <99910126+SNex21@users.noreply.github.com>
2025-01-28 22:28:31 +0300

    fix set tasks

--- apps/web/src/pages/(authenticated)/lesson/SetLessonPage/SetLessonPage.tsx
+++ apps/web/src/pages/(authenticated)/lesson/SetLessonPage/SetLessonPage.tsx
@@ -3,6 +3,7 @@ import { AnimatePresence } from "framer-motion";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import styles from "./SetLessonPage.module.scss";
+import { BackButton } from "@/lib/twa/components/BackButton";
 
 export default function SetLessonPage() {
   const params = useParams();
@@ -33,6 +34,7 @@ export default function SetLessonPage() {
 
   return (
     <AnimatePresence>
+      <BackButton onClick={() => navigate("/")} />
       <div className={styles.page}>
         <header className={styles.page__header}>
           <h2 className={styles.section__heading}>Настрой свою тренировку</h2>
