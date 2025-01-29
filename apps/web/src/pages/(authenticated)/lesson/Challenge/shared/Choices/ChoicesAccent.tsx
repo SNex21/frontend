32d07253e0f090b5bbc988171a3bebbf23a6bf52
Jacob <stupnikovd51@gmail.com>
2025-01-29 18:02:52 +0300

    UH_EPTA

--- apps/web/src/pages/(authenticated)/lesson/Challenge/shared/Choices/ChoicesAccent.tsx
+++ apps/web/src/pages/(authenticated)/lesson/Challenge/shared/Choices/ChoicesAccent.tsx
@@ -28,32 +28,46 @@ const ChoicesAccent: React.FC<ChoicesAccentProps> = ({ choices, currentChoice, s
     return null;
   }
 
+  // Массив гласных букв
+  const vowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
+
   return (
     <div className={styles.choices}>
-      {choices.map((choice, i) => (
-        <ChoiceAccent
-          key={`${choice.text}-${i}`}
-          choice={choice}
-          onSelect={!state?.submitted ? () => setChoice(choice) : undefined}
-          isSelected={choice === currentChoice}
-          state={state}
-        />
-      ))}
+      {choices.map((choice, i) => {
+        // Проверяем, является ли буква гласной
+        const isVowel = vowels.includes(choice.text.toLowerCase());
+
+        return (
+          <ChoiceAccent
+            key={`${choice.text}-${i}`}
+            choice={choice}
+            onSelect={isVowel && !state?.submitted ? () => setChoice(choice) : undefined}
+            isSelected={choice === currentChoice}
+            state={state}
+          />
+        );
+      })}
     </div>
   );
 };
 
 const ChoiceAccent: React.FC<ChoiceAccentProps> = ({ choice, onSelect, state, isSelected }) => {
+  // Массив гласных букв
+  const vowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
+  const isVowel = vowels.includes(choice?.text.toLowerCase());
+
   return (
-    <Haptic type={"impact"} value={"medium"} disabled={state?.submitted} asChild>
+    <Haptic type={"impact"} value={"medium"} disabled={!isVowel || state?.submitted} asChild>
       <div
         className={cn(styles.choice, {
           [styles["choice_not-submitted"]!]: !state?.submitted,
           [styles.choice_selected!]: isSelected,
           [styles.choice_right!]: isSelected && state?.submitted && !state?.wrong,
+          [styles.choice_disabled!]: !isVowel, // Добавляем класс для неактивных букв
         })}
         role="radio"
         onClick={onSelect}
+        aria-disabled={!isVowel} // Добавляем атрибут для доступности
       >
         <span className={styles.choice__text}>{choice?.text}</span>
         {isSelected && (
@@ -67,5 +81,4 @@ const ChoiceAccent: React.FC<ChoiceAccentProps> = ({ choice, onSelect, state, is
     </Haptic>
   );
 };
-
 export { ChoicesAccent, ChoiceAccent };
