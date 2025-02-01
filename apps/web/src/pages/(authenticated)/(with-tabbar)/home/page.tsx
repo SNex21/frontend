6c5a47f324f2dc3f94f688c8b58a7beda322b73b
Producer <99910126+SNex21@users.noreply.github.com>
2025-02-01 12:37:33 +0300

    fix slider

--- apps/web/src/pages/(authenticated)/(with-tabbar)/home/page.tsx
+++ apps/web/src/pages/(authenticated)/(with-tabbar)/home/page.tsx
@@ -3,8 +3,24 @@ import { GreetingSection } from "./GreetingSection";
 import styles from "./Home.module.scss";
 import { LessonsSection } from "./LessonsSection";
 import { WorkoutSection } from "./WorkoutSection";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  // Используем useEffect для выполнения кода при монтировании компонента
+  useEffect(() => {
+    if (window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+
+      // Запрещаем закрытие приложения через жесты
+      tg.MainButton.preventClosing();
+
+      // Опционально: Разрешаем закрытие при размонтировании компонента
+      return () => {
+        tg.MainButton.allowClosing();
+      };
+    }
+  }, []); // Пустой массив зависимостей означает, что код выполнится только один раз при загрузке
+
   return (
     <div className={pageStyles.main}>
       <div className={styles.content}>
