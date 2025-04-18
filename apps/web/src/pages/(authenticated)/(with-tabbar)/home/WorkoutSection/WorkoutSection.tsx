cfaefe348c2fb55547421ed8f49830aeb853dc09
Producer <99910126+SNex21@users.noreply.github.com>
2025-04-18 17:34:10 +0300

    add mistake change

--- apps/web/src/pages/(authenticated)/(with-tabbar)/home/WorkoutSection/WorkoutSection.tsx
+++ apps/web/src/pages/(authenticated)/(with-tabbar)/home/WorkoutSection/WorkoutSection.tsx
@@ -102,22 +102,22 @@ const WorkoutSection: FC = () => {
               icon={<FlexedBicepsEmoji size={25} />}
               href={"/set/lesson"}
             />
-            {user.mistake ? (            
-              <div className={styles.row}>
+            <div className={styles.row}>
+            {user.mistake ? (
               <WorkoutCard
                 title="Практика ошибок"
                 icon={<WarningEmoji size={25} />}
                 isSm
                 href={"/set/lesson/mistakes"}
-              />) : (
-                <div className={styles.row}>
-              <WorkoutCard
-                title="У тебя пока нет ошибок!"
-                icon={<WarningEmoji size={25} />}
-                isSm
-                href={"/set/lesson/mistakes"}
               />
-              )}
+            ):(
+            <WorkoutCard
+              title="У тебя пока нет ошибок"
+              icon={<WarningEmoji size={25} />}
+              isSm
+              href={"/set/lesson/mistakes"}
+            />
+            )}
               {showHardLessonButton && (
                 <WorkoutCard
                   title="Самые сложные"
