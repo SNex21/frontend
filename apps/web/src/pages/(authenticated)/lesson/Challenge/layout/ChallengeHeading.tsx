35c791014d63b4475612a63eb9914b0d8096a6bd
Producer <99910126+SNex21@users.noreply.github.com>
2025-04-18 13:35:48 +0300

    add report link

--- apps/web/src/pages/(authenticated)/lesson/Challenge/layout/ChallengeHeading.tsx
+++ apps/web/src/pages/(authenticated)/lesson/Challenge/layout/ChallengeHeading.tsx
@@ -13,20 +13,22 @@ const ChallengeHeading: React.FC<ChallengeHeadingProps> = ({ challenge, children
   }
 
   return (
-    <div className={styles.heading}>
-      {(challenge.isWorkOnMistakes || challenge.isLocalWom) && <p>Исправляем ошибки</p>}
-      {challenge.isHard && <p>Сложное задание</p>}
-      <h1 className={styles.heading__title}>{children}</h1>
-    </div>
-    <a
-      // href={`${import.meta.env.TELEGRAM_BOT_LINK}?start=report`} 
-      href="https://t.me/Example_chat_courses_bot?start=report"
-      target="_blank"
-      rel="noopener noreferrer"
-      className={styles.submit__panel__heading__report}
-    >
-      Сообщить об ошибке
-  </a>
+    <>
+      <div className={styles.heading}>
+        {(challenge.isWorkOnMistakes || challenge.isLocalWom) && <p>Исправляем ошибки</p>}
+        {challenge.isHard && <p>Сложное задание</p>}
+        <h1 className={styles.heading__title}>{children}</h1>
+      </div>
+      <a
+        // href={`${import.meta.env.TELEGRAM_BOT_LINK}?start=report`} 
+        href="https://t.me/Example_chat_courses_bot?start=report"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.submit__panel__heading__report}
+      >
+        Сообщить об ошибке
+    </a>
+  </>
   );
 };
 
