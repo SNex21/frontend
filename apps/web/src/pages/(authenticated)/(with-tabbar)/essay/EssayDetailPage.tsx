e12b88ff5295fefeee13c81f0efd611826424423
Producer <99910126+SNex21@users.noreply.github.com>
2025-05-03 02:57:42 +0300

    fix essays

--- apps/web/src/pages/(authenticated)/(with-tabbar)/essay/EssayDetailPage.tsx
+++ apps/web/src/pages/(authenticated)/(with-tabbar)/essay/EssayDetailPage.tsx
@@ -1,30 +1,37 @@
 import styles from "./EssayDetail.module.scss";
+import { BackButton } from "@/lib/twa/components/BackButton";
+import { useNavigate } from "react-router-dom";
 
 export default function EssayDetailPage() {
   const handleBuy = () => {
     alert("Вы купили сочинение!");
   };
+  const navigate = useNavigate();
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.card}>
-        <img
-          className={styles.image}
-          src="https://img.goodfon.ru/wallpaper/big/8/4e/badfon-full-hd-cvety-priroda-vesna.webp"
-          alt="Тургенев, Как быть человеком?"
-        />
-        <div className={styles.content}>
-          <h1 className={styles.title}>Тургенев, “Как быть человеком?”</h1>
-          <p className={styles.description}>
-            Как быть человеком – очень интересный вопрос, который задает Тургенев в своем рассказе.
-            Попробуй понять Тургенева вместе с нами.
-          </p>
+    <>
+      <BackButton onClick={() => navigate("/essay")} />
+      <div className={styles.wrapper}>
+        
+        <div className={styles.card}>
+          <img
+            className={styles.image}
+            src="https://img.goodfon.ru/wallpaper/big/8/4e/badfon-full-hd-cvety-priroda-vesna.webp"
+            alt="Тургенев, Как быть человеком?"
+          />
+          <div className={styles.content}>
+            <h1 className={styles.title}>Тургенев, “Как быть человеком?”</h1>
+            <p className={styles.description}>
+              Как быть человеком – очень интересный вопрос, который задает Тургенев в своем рассказе.
+              Попробуй понять Тургенева вместе с нами.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <button className={styles.buyButton} onClick={handleBuy}>
-        КУПИТЬ
-      </button>
-    </div>
+        <button className={styles.buyButton} onClick={handleBuy}>
+          КУПИТЬ
+        </button>
+      </div>
+    </>
   );
 }
